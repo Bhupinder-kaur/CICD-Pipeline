d031d1fe0d9e1ac8bd234da109cab2cf1868d024
Bhupinder Kaur <sbkaur6875@gmail.com>
2025-05-13 16:07:41 -0400

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ from sklearn.metrics import confusion_matrix, precision_score, f1_score, recall_
 sns.set(style='white')
 
 # Load Data
-dataset = pd.read_csv(r'S:\Data Science Prakash Senapati\MyWorks\1)MyPracticals\18)CICD Projects\iris.csv')
+dataset = pd.read_csv('iris.csv')
 
 # Feature names (Ensure no extra spaces or parentheses)
 dataset.columns = [colname.strip(' (cm)').replace(" ", "_") for colname in dataset.columns.tolist()]
